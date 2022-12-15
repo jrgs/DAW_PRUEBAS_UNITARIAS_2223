d3e2d1b2979a90f618d241ebeace952e1d94fd2c
BMS22-23 <bresen@alu.edu.gva.es>
2022-12-15 19:59:19 +0100

    Errores corregidos.

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,19 +12,19 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        private double saldo = 1000;  // Saldo inicial de la cuenta, 1000€
+        private double saldo_BMS2223 = 1000;  // Saldo inicial de la cuenta, 1000€
 
         public Form1()
         {
             InitializeComponent();
-            txtSaldo.Text = saldo.ToString();
+            txtSaldo.Text = saldo_BMS2223.ToString();
             txtCantidad.Text = "0";
         }
 
-        private bool realizarReintegro(double cantidad) 
+        private bool realizarReintegro(double cantidad_BMS2223) 
         {
-            if (cantidad > 0 && saldo > cantidad) {
-                saldo -= cantidad;
+            if (cantidad_BMS2223 > 0 && saldo_BMS2223 >= cantidad_BMS2223) {
+                saldo_BMS2223 -= cantidad_BMS2223;
                 return true;
             }
             return false;
@@ -32,7 +32,7 @@ namespace WindowsFormsApp1
 
         private void realizarIngreso(double cantidad) {
             if (cantidad > 0)
-                saldo += cantidad;
+                saldo_BMS2223 += cantidad;
         }
 
         private void btOperar_Click(object sender, EventArgs e)
@@ -45,10 +45,17 @@ namespace WindowsFormsApp1
             {
                 if (realizarReintegro(cantidad) == false)  // No se ha podido completar la operación, saldo insuficiente?
                     MessageBox.Show("No se ha podido realizar la operación (¿Saldo insuficiente?)");
+
+               else
+                    txtSaldo.Text = saldo_BMS2223.ToString();
+            }
+            else if (radioButton1.Checked)
+            {
+                realizarIngreso(cantidad);
+                txtSaldo.Text = saldo_BMS2223.ToString();
             }
             else
-                realizarIngreso(cantidad);
-            txtSaldo.Text = saldo.ToString();
+                MessageBox.Show("Debe elegir o ingreso o reintegro.");
         }
     }
 }
