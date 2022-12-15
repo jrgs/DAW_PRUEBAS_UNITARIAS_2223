b0765a47b127d7de02c7ed3beeaec3d46b374342
Antonio José Picó Pina <antpicpin@alu.edu.gva.es>
2022-12-15 19:47:15 +0100

    Corrección de errores

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,43 +12,51 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        private double saldo = 1000;  // Saldo inicial de la cuenta, 1000€
+        private double saldoAJPP22 = 1000;  // Saldo inicial de la cuenta, 1000€
 
         public Form1()
         {
             InitializeComponent();
-            txtSaldo.Text = saldo.ToString();
+            txtSaldo.Text = saldoAJPP22.ToString();
             txtCantidad.Text = "0";
         }
 
-        private bool realizarReintegro(double cantidad) 
+        private bool realizarReintegro(double cantidadAJPP22) 
         {
-            if (cantidad > 0 && saldo > cantidad) {
-                saldo -= cantidad;
+            if (cantidadAJPP22 > 0 && saldoAJPP22 >= cantidadAJPP22) {
+                saldoAJPP22 -= cantidadAJPP22;
                 return true;
             }
             return false;
         }
 
-        private void realizarIngreso(double cantidad) {
-            if (cantidad > 0)
-                saldo += cantidad;
+        private void realizarIngreso(double cantidadAJPP22) {
+            if (cantidadAJPP22 > 0)
+                saldoAJPP22 += cantidadAJPP22;
         }
 
         private void btOperar_Click(object sender, EventArgs e)
         {
-            double cantidad = Convert.ToDouble(txtCantidad.Text); // Cogemos la cantidad del TextBox y la pasamos a número
-            if (cantidad < 0) {
-                MessageBox.Show("Cantidad no válidá, sólo se admiten cantidades positivas.");
-            }
-            if (rbReintegro.Checked)
+            double cantidadAJPP22 = Convert.ToDouble(txtCantidad.Text); // Cogemos la cantidad del TextBox y la pasamos a número
+            if (cantidadAJPP22 < 0) {
+                MessageBox.Show("Cantidad no válida, sólo se admiten cantidades positivas.");
+            } else
             {
-                if (realizarReintegro(cantidad) == false)  // No se ha podido completar la operación, saldo insuficiente?
-                    MessageBox.Show("No se ha podido realizar la operación (¿Saldo insuficiente?)");
+                if (rbReintegro.Checked)
+                {
+                    if (realizarReintegro(cantidadAJPP22) == false)  // No se ha podido completar la operación, saldo insuficiente?
+                        MessageBox.Show("No se ha podido realizar la operación (¿Saldo insuficiente?)");
+                }
+                else if (rbIngreso.Checked)
+                {
+                    realizarIngreso(cantidadAJPP22);
+                }
+                else
+                {
+                    MessageBox.Show("Debe seleccionar una opción");
+                }
+                txtSaldo.Text = saldoAJPP22.ToString();
             }
-            else
-                realizarIngreso(cantidad);
-            txtSaldo.Text = saldo.ToString();
         }
     }
 }
