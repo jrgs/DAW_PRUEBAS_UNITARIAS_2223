f3c31ef7dc824d7d5286dae8791364508e63b05a
ROBERTO ESPADIÑA FERNANDEZ <robespfer@alu.edu.gva.es>
2022-12-15 19:42:39 +0100

    REF1DAWY Examen

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -11,44 +11,47 @@ using System.Windows.Forms;
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
+
     {
-        private double saldo = 1000;  // Saldo inicial de la cuenta, 1000€
+       
+
+        private double saldoREF1DAWY = 1000;  // Saldo inicial de la cuenta, 1000€
 
         public Form1()
         {
             InitializeComponent();
-            txtSaldo.Text = saldo.ToString();
+            txtSaldo.Text = saldoREF1DAWY.ToString();
             txtCantidad.Text = "0";
         }
 
-        private bool realizarReintegro(double cantidad) 
+        private bool realizarReintegro(double CantidadREF1DAWY) 
         {
-            if (cantidad > 0 && saldo > cantidad) {
-                saldo -= cantidad;
+            if (CantidadREF1DAWY > 0 && saldoREF1DAWY > CantidadREF1DAWY) {
+                saldoREF1DAWY = saldoREF1DAWY - CantidadREF1DAWY;
                 return true;
             }
             return false;
         }
 
-        private void realizarIngreso(double cantidad) {
-            if (cantidad > 0)
-                saldo += cantidad;
+        private void realizarIngreso(double CantidadREF1DAWY) {
+            if (CantidadREF1DAWY > 0)
+                saldoREF1DAWY += CantidadREF1DAWY;
         }
 
         private void btOperar_Click(object sender, EventArgs e)
         {
-            double cantidad = Convert.ToDouble(txtCantidad.Text); // Cogemos la cantidad del TextBox y la pasamos a número
-            if (cantidad < 0) {
+            double CantidadREF1DAWY = Convert.ToDouble(txtCantidad.Text); // Cogemos la cantidad del TextBox y la pasamos a número
+            if (CantidadREF1DAWY < 0) {
                 MessageBox.Show("Cantidad no válidá, sólo se admiten cantidades positivas.");
             }
             if (rbReintegro.Checked)
             {
-                if (realizarReintegro(cantidad) == false)  // No se ha podido completar la operación, saldo insuficiente?
+                if (realizarReintegro(CantidadREF1DAWY) == false)  // No se ha podido completar la operación, saldo insuficiente?
                     MessageBox.Show("No se ha podido realizar la operación (¿Saldo insuficiente?)");
             }
             else
-                realizarIngreso(cantidad);
-            txtSaldo.Text = saldo.ToString();
+                realizarIngreso(CantidadREF1DAWY);
+            txtSaldo.Text = saldoREF1DAWY.ToString();
         }
     }
 }
