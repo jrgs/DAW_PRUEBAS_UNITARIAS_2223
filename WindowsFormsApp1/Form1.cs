f9f6fced65f5c56b96943e9ee0132ed22ad4a57d
borrajo21457 <oscborgra@alu.edu.gva.es>
2022-12-15 19:58:21 +0100

    Correccion codigo OskarBorrajo

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,43 +12,46 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        private double saldo = 1000;  // Saldo inicial de la cuenta, 1000€
+        private double saldo_OskBorr = 1000;  // Saldo inicial de la cuenta, 1000€
 
         public Form1()
         {
             InitializeComponent();
-            txtSaldo.Text = saldo.ToString();
+            txtSaldo.Text = saldo_OskBorr.ToString();
             txtCantidad.Text = "0";
         }
 
-        private bool realizarReintegro(double cantidad) 
+        private bool realizarReintegro(double cantidad_OskBorr) 
         {
-            if (cantidad > 0 && saldo > cantidad) {
-                saldo -= cantidad;
+            if (cantidad_OskBorr > 0 && saldo_OskBorr >= cantidad_OskBorr) {
+                saldo_OskBorr -= cantidad_OskBorr;
                 return true;
             }
             return false;
         }
 
-        private void realizarIngreso(double cantidad) {
-            if (cantidad > 0)
-                saldo += cantidad;
+        private void realizarIngreso(double cantidad_OskBorr) {
+            if (cantidad_OskBorr > 0)
+                saldo_OskBorr += cantidad_OskBorr;
         }
 
         private void btOperar_Click(object sender, EventArgs e)
         {
-            double cantidad = Convert.ToDouble(txtCantidad.Text); // Cogemos la cantidad del TextBox y la pasamos a número
-            if (cantidad < 0) {
+            double cantidad_OskBorr = Convert.ToDouble(txtCantidad.Text); // Cogemos la cantidad del TextBox y la pasamos a número
+            if (cantidad_OskBorr < 0) {
                 MessageBox.Show("Cantidad no válidá, sólo se admiten cantidades positivas.");
             }
-            if (rbReintegro.Checked)
-            {
-                if (realizarReintegro(cantidad) == false)  // No se ha podido completar la operación, saldo insuficiente?
-                    MessageBox.Show("No se ha podido realizar la operación (¿Saldo insuficiente?)");
-            }
             else
-                realizarIngreso(cantidad);
-            txtSaldo.Text = saldo.ToString();
+            {   if (rbReintegro.Checked)
+                {
+                    if (realizarReintegro(cantidad_OskBorr) == false)  // No se ha podido completar la operación, saldo insuficiente?
+                        MessageBox.Show("No se ha podido realizar la operación (¿Saldo insuficiente?)");
+               
+                    else
+                        realizarIngreso(cantidad_OskBorr);
+                    txtSaldo.Text = saldo_OskBorr.ToString();
+            }
+            MessageBox.Show("No se ha podido realizar la operación (¿ingreso o reintegro?)");
         }
     }
 }
