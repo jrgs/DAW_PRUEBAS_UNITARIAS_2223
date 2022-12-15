429510987aaaca6f36fedc851aa0a2f6f27b081d
FJRM1DAW <frarodmed@alu.edu.gva.es>
2022-12-15 19:46:57 +0100

    Correcion de errores. Francisco Jose Rodenas Medina. Entornos de desarrollo. 1ºDAW

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,43 +12,54 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        private double saldo = 1000;  // Saldo inicial de la cuenta, 1000€
+        private double saldoFJRM1DAW = 1000;  // Saldo inicial de la cuenta, 1000€
 
         public Form1()
         {
             InitializeComponent();
-            txtSaldo.Text = saldo.ToString();
+            txtSaldo.Text = saldoFJRM1DAW.ToString();
             txtCantidad.Text = "0";
         }
 
-        private bool realizarReintegro(double cantidad) 
+        private bool realizarReintegro(double cantidadFJRM1DAW) 
         {
-            if (cantidad > 0 && saldo > cantidad) {
-                saldo -= cantidad;
+            if (cantidadFJRM1DAW > 0 && saldoFJRM1DAW >= cantidadFJRM1DAW) {
+                saldoFJRM1DAW -= cantidadFJRM1DAW;
                 return true;
+                
             }
             return false;
         }
 
-        private void realizarIngreso(double cantidad) {
-            if (cantidad > 0)
-                saldo += cantidad;
+        private void realizarIngreso(double cantidadFJRM1DAW) {
+            if (cantidadFJRM1DAW > 0)
+                saldoFJRM1DAW += cantidadFJRM1DAW;
+            else
+                saldoFJRM1DAW -= cantidadFJRM1DAW;
         }
 
         private void btOperar_Click(object sender, EventArgs e)
         {
-            double cantidad = Convert.ToDouble(txtCantidad.Text); // Cogemos la cantidad del TextBox y la pasamos a número
-            if (cantidad < 0) {
-                MessageBox.Show("Cantidad no válidá, sólo se admiten cantidades positivas.");
+            double cantidadFJRM1DAW = Convert.ToDouble(txtCantidad.Text); // Cogemos la cantidad del TextBox y la pasamos a número
+            if (cantidadFJRM1DAW < 0) || (cantidadFJRM1DAW > 0) && (realizarReintegro == false) {
+                MessageBox.Show("Cantidad no válida, sólo se admiten cantidades positivas.");
             }
             if (rbReintegro.Checked)
             {
-                if (realizarReintegro(cantidad) == false)  // No se ha podido completar la operación, saldo insuficiente?
+                if (realizarReintegro(cantidadFJRM1DAW) == false)  // No se ha podido completar la operación, saldo insuficiente?
                     MessageBox.Show("No se ha podido realizar la operación (¿Saldo insuficiente?)");
+                else 
+                    txtSaldo.Text = saldoFJRM1DAW.ToString();
             }
             else
-                realizarIngreso(cantidad);
-            txtSaldo.Text = saldo.ToString();
-        }
+                if (rbIngreso.Checked)
+            {
+                realizarIngreso(cantidadFJRM1DAW);
+                txtSaldo.Text = saldoFJRM1DAW.ToString();
+            }
+            else
+                MessageBox.Show("Seleccione Ingreso o Reintegro");
+
+    }
     }
 }
