e2bee035e53cd363c91292aa5a8d067f976c4d8c
ANDBAN2223 <andban2@alu.edu.gva.es>
2023-02-21 21:44:52 +0100

    Crear y añadir un proyecto de pruebas

---
+++ GestionBancariaTest/GestionBancariaTestsANDBAN2223.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using GestionBancariaAppNS;
+
+namespace GestionBancariaTest
+{
+    [TestClass]
+    public class GestionBancariaTestsANDBAN2223
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ GestionBancariaTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GestionBancariaTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GestionBancariaTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("595c8849-1729-4be4-8f03-ec17e7fd134e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
